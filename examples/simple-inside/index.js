a011a3291d780d397feb246609b0100e7c0df44b
Enrique Ortiz <enrique@aragon.one>
2020-03-02 10:42:35 -0400

    Examples: Modify example to reflect a real-life use-case (#4)

--- examples/simple-inside/index.js
+++ examples/simple-inside/index.js
@@ -4,41 +4,86 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import { Inside, useInside } from 'use-inside'
 
-function Card({ insideName }) {
-  const [inside, data] = useInside(insideName)
+function Card({ header, children, footer, color = 'white' }) {
+  // Inside elements can be seen as a way to declare “slots” in the component.
+  // They can be seen as part of its API, and are particularily useful for reusable
+  // components that need to behave differently based on where they are being used.
   return (
-    <div
-      style={{
-        width: '300px',
-        height: '200px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        ...data,
-      }}
-    >
-      <h2>{inside && insideName}</h2>
-    </div>
+    <Inside name="Card" data={{ cardColor: color }}>
+      <section
+        style={{
+          background: color,
+          textAlign: 'center',
+          marginBottom: '8px',
+        }}
+      >
+        <h1>
+          <Inside name="Card:header">{header}</Inside>
+        </h1>
+        <div>
+          <Inside name="Card:content">{children}</Inside>
+        </div>
+        <footer>
+          <Inside name="Card:footer">{footer}</Inside>
+        </footer>
+      </section>
+    </Inside>
   )
 }
 
 Card.propTypes = {
-  insideName: PropTypes.string.isRequired,
+  header: PropTypes.string,
+  children: PropTypes.node,
+  footer: PropTypes.node,
+  color: PropTypes.string,
+}
+
+function Button({ label }) {
+  // true if Button is being used inside of Card
+  const [insideCard, cardData] = useInside('Card')
+  const buttonColor = insideCard ? cardData.cardColor : 'grey'
+
+  // true if Button is being used inside of the Card’s footer
+  const [insideCardFooter] = useInside('Card:footer')
+
+  return (
+    <button
+      style={{
+        background: buttonColor,
+        fontWeight: insideCard ? 'bold' : 'normal',
+        width: insideCardFooter ? '100%' : 'auto',
+      }}
+    >
+      {label}
+    </button>
+  )
+}
+
+Button.propTypes = {
+  label: PropTypes.string,
 }
 
 function App() {
+  // The Button’s text will be bold and have “tomato” as a background color
+  const card1 = (
+    <Card color="tomato">
+      <Button label="Card button" />
+    </Card>
+  )
+
+  // Button will get a 100% width and have “papayawhip” as a background color
+  const card2 = (
+    <Card color="papayawhip" footer={<Button label="Footer button" />} />
+  )
+
+  // Default Button
+  const button = <Button label="Normal button" />
+
   return (
     <>
-      <h1>use-inside</h1>
-      <Inside name="Card:red" data={{ backgroundColor: 'red' }}>
-        <Card insideName="Card:red" />
-      </Inside>
-      <Inside
-        name="Card:blue"
-        data={{ backgroundColor: 'blue', marginTop: '24px' }}
-      >
-        <Card insideName="Card:blue" />
-      </Inside>
+      {card1}
+      {card2}
+      {button}
     </>
   )
 }
